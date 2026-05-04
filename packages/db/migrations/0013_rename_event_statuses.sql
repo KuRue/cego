3ffbe4b8cadc23a2ae39fb2a62bc925b1af72023
KuRue <ambpc.net@gmail.com>
2026-05-03 23:54:49 -0400

    Fix migration 0013: drop default before altering column type

--- packages/db/migrations/0013_rename_event_statuses.sql
+++ packages/db/migrations/0013_rename_event_statuses.sql
@@ -1,13 +1,11 @@
+ALTER TABLE events ALTER COLUMN status DROP DEFAULT;
 CREATE TYPE event_status_new AS ENUM ('draft', 'show', 'closed', 'archived');
-
 ALTER TABLE events ALTER COLUMN status TYPE event_status_new USING
   CASE status
     WHEN 'open' THEN 'show'
     WHEN 'full' THEN 'show'
     ELSE status::text
   END::event_status_new;
-
 ALTER TABLE events ALTER COLUMN status SET DEFAULT 'draft';
-
 DROP TYPE event_status;
 ALTER TYPE event_status_new RENAME TO event_status;
