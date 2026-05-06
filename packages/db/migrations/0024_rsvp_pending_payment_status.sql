fd0b454196eae5304f89b829ec75df7caf9861a8
KuRue <ambpc.net@gmail.com>
2026-05-06 17:46:14 -0400

    Fix migration journal: mark all manual migrations as applied, fix 0024 enum

--- packages/db/migrations/0024_rsvp_pending_payment_status.sql
+++ packages/db/migrations/0024_rsvp_pending_payment_status.sql
@@ -1,2 +1,6 @@
-ALTER TYPE rsvp_status ADD VALUE IF NOT EXISTS 'expired';
+DO $$ BEGIN
+  ALTER TYPE rsvp_status ADD VALUE IF NOT EXISTS 'expired';
+EXCEPTION WHEN others THEN
+  NULL;
+END $$;
 ALTER TABLE rsvps ADD COLUMN IF NOT EXISTS payment_deadline_at timestamptz;
